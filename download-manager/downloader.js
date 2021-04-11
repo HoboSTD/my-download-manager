4db5a366fbd0d41936e8c8634cd777bf3ee77e8f
Jonathan Clark <jonathan@clarknet.com.au>
2021-04-11 16:31:09 +1000

    Made small changes so that only pictures get re-routed to a specific url.

--- download-manager/downloader.js
+++ download-manager/downloader.js
@@ -2,5 +2,8 @@
 $('a[download]').each(function() {
     // change this link to instead download a funny image
     // this can instead be replace with a url to the malicious server
-    $(this).attr("href", "/static/download.jpeg");
+    url = $(this).attr("href")
+    if (url == "/static/doomer.jpg") {
+        $(this).attr("href", "/static/download.jpeg");
+    }
 });
